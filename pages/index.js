9c634fd0ca459cf2155cbd5f921e374fe98516e2
Victor Fantucci <victor.v.fantucci@gmail.com>
2025-10-31 20:58:39 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home()
+{
+    return <h1>"Eu juro pela minha Vida e pelo meu amor por ela que nunca irei viver em função de outro homem, nem vou pedir a outro homem que viva em função de mim."</h1>
+}
+
+export default Home;
