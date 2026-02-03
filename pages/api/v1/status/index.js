b907d54f77a745978f9258312e4be93cbf4ed428
Victor Fantucci <victor.v.fantucci@gmail.com>
2026-02-03 20:56:35 -0300

    Adiciona os arquivos `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.setHeader("Content-Type", "application/json; charset=utf-8");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
 
